f53dff5bcf5c8bdb9aa521fdcf41ffa3428fee49
princecoder51002 <83036763+princecoder51002@users.noreply.github.com>
2021-05-04 12:33:34 +0530

    create users functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,19 @@ class UsersController < ApplicationController
         @user = User.new
     end
     
+    def create
+      @user = User.new(user_params)
+      if @user.save 
+        flash[:notice] = "Welome To The Prince Blog #{@user.username}, You Have Successfully Signed Up!!!"
+        redirect_to articles_path
+      else 
+        render 'new'
+      end
+    end
+    
+    private
+    def user_params
+           params.require(:user).permit(:username, :email, :password)
+    end
 
 end
