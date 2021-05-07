b176f2abbc0778a8a6beee858a5956ab57f192d0
princecoder51002 <83036763+princecoder51002@users.noreply.github.com>
2021-05-07 11:11:17 +0530

    create and destroy sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,21 @@ class SessionsController < ApplicationController
     end
 
     def create 
-
+         user = User.find_by(email: params[:session][:email].downcase)
+         if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+         flash[:notice] = "Logged In Succesfully!!"
+         redirect_to user
+         else
+            flash.now[:alert] = "Invalid Email and Password"
+            render 'new'
+         end
     end
 
     def destroy 
-
+        session[:user_id] = nil 
+        flash[:notice] = "Logged out!! , Thanks for using prince blogs!!!"
+        redirect_to root_path
     end
     
 
