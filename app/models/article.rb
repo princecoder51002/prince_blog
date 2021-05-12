0020e3d84cf49c94d23f0102d5f5b7a0f588bca3
princecoder51002 <83036763+princecoder51002@users.noreply.github.com>
2021-05-12 12:31:49 +0530

    many to many association
    from back-end

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ApplicationRecord
     belongs_to :user
 
+    has_many :article_categories
+    has_many :categories, through: :article_categories
+
     validates :title, presence: true, length: {minimum: 6, maximum: 100}
     validates :description, presence: true, length: {minimum: 10, maximum: 500} # this ensures the presence of title of any article to be saved
 
